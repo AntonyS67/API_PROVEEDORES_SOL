420e67cc370613105b9bf25a220f395e46e04043
Antoni Silva <ansir.manuel@gmail.com>
2024-11-05 18:32:56 -0500

    Testing

---
+++ Testing/SupplierTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Testing
+{
+    public class SupplierTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+        public SupplierTests(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+        [Fact]
+        public async Task ListSupplier()
+        {
+            var response = await _httpClient.GetAsync("/api/suppliers/list");
+
+            //Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.False(string.IsNullOrEmpty(content));
+        }
+    }
+}
--- Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
+++ Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+767df03898191c9fd1bcbd84728b9621c9ef2b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
